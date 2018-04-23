de05e8027bb9ab641083a507ffe168aa09b99eea
dskeshav <keshavds94@gmail.com>
2018-04-23 17:37:07 +0530

    Minor changes

---
+++ src/main/java/Phptravels/App.java
@@ -0,0 +1,15 @@
+package Phptravels;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("Integrated");
+    }
+}
